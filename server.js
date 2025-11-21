7030f31f6c16622d0bf56c62801b06c2ed65d87b
VENU GOPAL RAMIREDDY <venugopalrvg9@gmail.com>
2025-11-21 16:20:50 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,5 @@
+function clickEvent(){
+	var button = document.getElementById("clickEvent");
+	var output = document.getElementById("output");
+	output.innerHTML = "You Have Clicked Button on Click Event Listener";
+}
